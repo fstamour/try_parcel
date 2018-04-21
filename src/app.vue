a8d3d0ba8329092990ee0b115572151073333605
Francis St-Amour <fr.stamour@gmail.com>
2018-04-21 14:39:02 -0400

    Add a working vue component

---
+++ src/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app"><h1>bla</h1></div>
+</template>
+
+<script>
+ export default {
+   data() {
+     return { };
+   }
+ }
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.write("ble");
+import Vue from 'vue/dist/vue.js';
+import App from './app';
 
+Vue.config.productionTip = false;
 
+const app = window.addEventListener('load', () => {
+    new Vue({
+        el: '#root',
+        components: { App },
+        template: '<App/>'
+    });
+})
+
+export default app;
